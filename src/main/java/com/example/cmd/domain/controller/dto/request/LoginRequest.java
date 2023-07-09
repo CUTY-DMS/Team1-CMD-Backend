7b8453b9a95fbdd4a57c70cc4d9f8edb39524678
tae.y <0211ilyoil@gmail.com>
2023-07-09 16:57:04 +0900

    🗿 :: 마이페이지 완성

--- src/main/java/com/example/cmd/domain/controller/dto/request/LoginRequest.java
+++ src/main/java/com/example/cmd/domain/controller/dto/request/LoginRequest.java
@@ -3,9 +3,15 @@ package com.example.cmd.domain.controller.dto.request;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
+
 @Getter
 @NoArgsConstructor()
 public class LoginRequest {
+
+    @NotNull(message = "이메일을 입력하세요")
     private String email;
+
+    @NotNull(message = "비밀번호를 입력하세요")
     private String password;
 }
